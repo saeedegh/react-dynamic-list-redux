644a3f0186bd0dbcb9f766bb3894d049d7c12929
saeedeProfessional <worksaeede@gmail.com>
2021-10-25 20:58:24 +0330

    fixed card styles

--- src/components/Home/MainList.js
+++ src/components/Home/MainList.js
@@ -17,8 +17,38 @@ function MainList() {
       setLoading(true);
       setTimeout(() => {
         setList([
-          { id: 1, name: "Apple", price: 2.1, description: "normal apple" },
-          { id: 2, name: "Orrange", price: 1.6, description: "freezed" },
+          {
+            id: 1,
+            name: "Apple",
+            price: 2.1,
+            description: "normal apple",
+            image:
+              "https://static.libertyprim.com/files/familles/pomme-large.jpg?1569271834",
+          },
+          {
+            id: 2,
+            name: "Orrange",
+            price: 1.6,
+            description: "freezed",
+            image:
+              "https://5.imimg.com/data5/VN/YP/MY-33296037/orange-600x600-500x500.jpg",
+          },
+          {
+            id: 3,
+            name: "banana",
+            price: 2.6,
+            description: "freezed",
+            image:
+              "http://barthfruit.ch/custom/images/contentBilder/big/img_0658_banane.jpg",
+          },
+          {
+            id: 4,
+            name: "strawberry",
+            price: 1,
+            description: "freezed",
+            image:
+              "https://img.pixers.pics/pho_wat(s3:700/FO/70/41/90/18/700_FO70419018_d5aebe501709c7e5ee799943e3581cb1.jpg,700,690,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,640,jpg)/wall-murals-one-rich-strawberry-fruit.jpg.jpg",
+          },
         ]);
         setLoading(false);
       }, 1000);
